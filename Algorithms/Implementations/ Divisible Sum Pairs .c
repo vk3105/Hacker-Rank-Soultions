044b3da3a46871dcbabf82d40864a1a250ceb4cc
vk3105 <vipin3105@gmail.com>
2017-01-01 02:18:03 -0500

    Create  Divisible Sum Pairs .c

---
+++ Algorithms/Implementations/ Divisible Sum Pairs .c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    int k;
+    int i,j,count;
+    scanf("%d %d",&n,&k);
+    int *a = malloc(sizeof(int) * n);
+    for(int a_i = 0; a_i < n; a_i++){
+       scanf("%d",&a[a_i]);
+    }
+    count = 0;
+    for(i = 0; i < n-1; i++){
+        for(j = i+1; j < n; j++){
+            if((a[i]+a[j])%k == 0){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
+}

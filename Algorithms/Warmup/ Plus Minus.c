5d246cee66ad3148624ee646a74aa6e41c17a940
Vipin Kumar <vipin3105@gmail.com>
2017-01-08 21:03:46 -0500

    Create  Plus Minus.c

---
+++ Algorithms/Warmup/ Plus Minus.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    float i,j,k;
+    i = j = k = 0.0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+       if(arr[arr_i]==0)
+           i++;
+        if(arr[arr_i]>0)
+           j++;
+        if(arr[arr_i]<0)
+           k++;
+    }
+    
+    printf("%f\n",j/n);
+    printf("%f\n",k/n);
+    
+    
+    printf("%f\n",i/n);
+    return 0;
+}
